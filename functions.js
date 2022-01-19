d7539c32d4084f75e43657846ce9b7404d4ff08c
TAN JUN YU RIAN stu <tan_jun_yu_rian@s2021.ssts.edu.sg>
2022-01-19 14:19:11 +0800

    zave arse

--- functions.js
+++ functions.js
@@ -2,9 +2,12 @@ window.transitionToPage = function (href) {
   document.querySelector("body").style.opacity = 0;
   setTimeout(function () {
     window.location.href = href;
-  });
+  }, 550);
 };
 
 document.addEventListener("DOMContentLoaded", function (event) {
   document.querySelector("body").style.opacity = 5;
+  setTimeout(function () {
+    window.location.href = href;
+  }, 550);
 });
